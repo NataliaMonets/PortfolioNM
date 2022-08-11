192e1405272f3e8220d3e17d2d3775d1c0707406
Natalia Monets <natalia25.monets@gmail.com>
2022-08-11 15:17:41 +0300

    skills component style changed, home html and ts changed

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -8,6 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public showAbout = false;
+  public contactsArr = [
+    {
+      href: "https://t.me/Natalia_Monets",
+      icon: "assets/images/telegram1.svg"
+    },
+    {
+      href: "https://www.linkedin.com/in/natalia-monets-534baa198/",
+      icon: "assets/images/social-media-54.svg"
+    },
+    {
+      href: "https://www.facebook.com/nata.monets/",
+      icon: "assets/images/social-media-54 (1).svg"
+    },
+    {
+      href: "https://github.com/NataliaMonets",
+      icon: "assets/images/social-media-54 (2).svg"
+    },
+    {
+      href: "mailto: nataliia.monets@gmail.com",
+      icon: "assets/images/gmail1.svg"
+    }
+  ]
 
   constructor() { }
 
